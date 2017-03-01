025a2124124824ad0877851009f1b27fca0ace33
zyj <867269369@qq.com>
2017-03-01 09:14:19 +0800

    这是在测试sourcetree

---
+++ build的副本.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zyj on 2016/11/23.
+ */
+({
+    baseUrl: "fw-module/js",
+    paths : {
+        fw :'fw',
+        main:'main',
+        paging:'paging'
+    },
+    name: "main",//这个是主入口
+    out: "build/paging.js"
+})
